e66decdbac2e099ab44b556659aa4d34cb2e8985
CleverlyDone <pierregober@gmail.com>
2022-03-09 02:50:37 -0800

    edit routes + build.zip

--- routes.js
+++ routes.js
@@ -3,7 +3,9 @@ const express = require("express");
 const uuid = require("uuid");
 
 const IS_OFFLINE = process.env.NODE_ENV !== "production";
-const STUDENTS_TABLE = process.env.TABLE;
+const TABLE_USER = process.env.TABLE_USER;
+const TABLE_CLASS = process.env.TABLE_CLASS;
+const TABLE_SEATS = process.env.TABLE_SEATS;
 
 const dynamoDb =
   IS_OFFLINE === true
@@ -15,23 +17,24 @@ const dynamoDb =
 
 const router = express.Router();
 
-router.get("/students", (req, res) => {
+/* START - USER TABLE */
+router.get("/users", (req, res) => {
   const params = {
-    TableName: STUDENTS_TABLE,
+    TableName: TABLE_USER,
   };
   dynamoDb.scan(params, (error, result) => {
     if (error) {
-      res.status(400).json({ error: "Error fetching the students" });
+      res.status(400).json({ error: "Error fetching the users" });
     }
     res.json(result.Items);
   });
 });
 
-router.get("/students/:id", (req, res) => {
+router.get("/users/:id", (req, res) => {
   const id = req.params.id;
 
   const params = {
-    TableName: STUDENTS_TABLE,
+    TableName: TABLE_USER,
     Key: {
       id,
     },
@@ -39,44 +42,47 @@ router.get("/students/:id", (req, res) => {
 
   dynamoDb.get(params, (error, result) => {
     if (error) {
-      res.status(400).json({ error: "Error retrieving Student" });
+      res.status(400).json({ error: "Error retrieving User" });
     }
     if (result.Item) {
       res.json(result.Item);
     } else {
-      res.status(404).json({ error: `Student with id: ${id} not found` });
+      res.status(404).json({ error: `User with id: ${id} not found` });
     }
   });
 });
 
-router.post("/students", (req, res) => {
-  const name = req.body.name;
+router.post("/users/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
   const id = uuid.v4();
 
   const params = {
-    TableName: STUDENTS_TABLE,
+    TableName: TABLE_USER,
     Item: {
       id,
-      name,
+      firstName,
+      lastName,
+      email,
     },
   };
 
   dynamoDb.put(params, (error) => {
     if (error) {
-      res.status(400).json({ error: "Could not create Student" });
+      res.status(400).json({ error: "Could not create User" });
     }
     res.json({
       id,
-      name,
     });
   });
 });
 
-router.delete("/student/:id", (req, res) => {
+router.delete("/user/:id", (req, res) => {
   const id = req.params.id;
 
   const params = {
-    TableName: STUDENTS_TABLE,
+    TableName: TABLE_USER,
     Key: {
       id,
     },
@@ -84,18 +90,18 @@ router.delete("/student/:id", (req, res) => {
 
   dynamoDb.delete(params, (error) => {
     if (error) {
-      res.status(400).json({ error: "Could not delete Student" });
+      res.status(400).json({ error: "Could not delete User" });
     }
     res.json({ success: true });
   });
 });
 
-router.put("/students", (req, res) => {
+router.put("/users", (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
 
   const params = {
-    TableName: STUDENTS_TABLE,
+    TableName: TABLE_USER,
     Key: {
       id,
     },
@@ -107,10 +113,16 @@ router.put("/students", (req, res) => {
 
   dynamoDb.update(params, (error, result) => {
     if (error) {
-      res.status(400).json({ error: "Could not update Student" });
+      res.status(400).json({ error: "Could not update User" });
     }
     res.json(result.Attributes);
   });
 });
+
+/* END - USER TABLE */
+
+/* START - CLASS TABLE */
+
+/* END - CLASS TABLE */
 
 module.exports = router;
